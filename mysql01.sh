f0045e811d70234a6be535bd3b1ad621402fce98
raj <raj.pradeep676@gmail.com>
2024-04-22 17:16:08 +0530

    mysql

--- mysql01.sh
+++ mysql01.sh
@@ -42,11 +42,10 @@ systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "starting the mysql"
 
 mysql -h db.pradeep17.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-
 if [ $? -ne 0 ]
-then 
-   mysql_secure_installation --set-root-pass ${mysql_root_passwordsql_root_password}
-       VALIDATE $? "MySQL Root password Setup"
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
